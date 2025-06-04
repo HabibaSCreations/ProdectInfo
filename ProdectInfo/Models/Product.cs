56027bedaec166e54247100e0aa1dd18648f6802
HabibaSCreations <habibaanjum9799@gmail.com>
2025-06-04 17:04:21 +0600

    Product

---
+++ ProdectInfo/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProdectInfo.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string MobileName { get; set; }
+        public string Image { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
